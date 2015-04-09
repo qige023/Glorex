1a5201ac95f0d0e07c7ed3d2565038faea70d8d8
vicky <yuqi.fan@foxmail.com>
2015-04-09 15:54:12 +0800

    add SOIL to support bmp texture loading

--- app/src/main/jni/Android.mk
+++ app/src/main/jni/Android.mk
@@ -1,1 +1,8 @@
+LOCAL_PATH := $(call my-dir)
+include $(CLEAR_VARS)
+LOCAL_MODULE    := ../tools/include/libSOIL
+LOCAL_SRC_FILES := ../tools/library/libSOIL.a
+include $(PREBUILT_STATIC_LIBRARY)
+include $(CLEAR_VARS)
+
 include $(call all-subdir-makefiles)
