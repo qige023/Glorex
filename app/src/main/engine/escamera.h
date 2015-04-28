254e9f287b008c188ed3116578e689ebbda532d4
vicky <yuqi.fan@foxmail.com>
2015-04-28 22:37:22 +0800

    code format

--- app/src/main/engine/escamera.h
+++ app/src/main/engine/escamera.h
@@ -4,12 +4,12 @@
 #include <vector>
 
 // GL Includes
-#include <GL/glew.h>
+#include <esutil.h>
 #include <glm/glm.hpp>
 #include <glm/gtc/matrix_transform.hpp>
 
 // Defines several possible options for camera movement. Used as abstraction to stay away from window-system specific input methods
-enum Camera_Movement {
+enum ESCamera_Movement {
     FORWARD, BACKWARD, LEFT, RIGHT
 };
 
@@ -21,7 +21,7 @@ const GLfloat SENSITIVTY = 0.25f;
 const GLfloat ZOOM = 45.0f;
 
 // An abstract camera class that processes input and calculates the corresponding Eular Angles, Vectors and Matrices for use in OpenGL
-class Camera {
+class ESCamera {
 public:
     // Camera Attributes
     glm::vec3 Position;
@@ -38,7 +38,7 @@ public:
     GLfloat Zoom;
 
     // Constructor with vectors
-    Camera(glm::vec3 position = glm::vec3(0.0f, 0.0f, 0.0f), glm::vec3 up =
+    ESCamera(glm::vec3 position = glm::vec3(0.0f, 0.0f, 0.0f), glm::vec3 up =
             glm::vec3(0.0f, 1.0f, 0.0f), GLfloat yaw = YAW, GLfloat pitch =
             PITCH) :
             Front(glm::vec3(0.0f, 0.0f, -1.0f)), MovementSpeed(SPEED), MouseSensitivity(
@@ -50,7 +50,7 @@ public:
         this->updateCameraVectors();
     }
     // Constructor with scalar values
-    Camera(GLfloat posX, GLfloat posY, GLfloat posZ, GLfloat upX, GLfloat upY,
+    ESCamera(GLfloat posX, GLfloat posY, GLfloat posZ, GLfloat upX, GLfloat upY,
             GLfloat upZ, GLfloat yaw, GLfloat pitch) :
             Front(glm::vec3(0.0f, 0.0f, -1.0f)), MovementSpeed(SPEED), MouseSensitivity(
                     SENSITIVTY), Zoom(ZOOM) {
@@ -68,7 +68,7 @@ public:
     }
 
     // Processes input received from any keyboard-like input system. Accepts input parameter in the form of camera defined ENUM (to abstract it from windowing systems)
-    void ProcessKeyboard(Camera_Movement direction, GLfloat deltaTime) {
+    void ProcessKeyboard(ESCamera_Movement direction, GLfloat deltaTime) {
         GLfloat velocity = this->MovementSpeed * deltaTime;
         if (direction == FORWARD)
             this->Position += this->Front * velocity;
